1a8d92ff2000b5cdd1cc7283db84b072b5d83851
ana204 <anagomes.lucia@hotmail.com>
2022-08-07 15:50:17 -0300

    quiz

--- src/script.js
+++ src/script.js
@@ -1,13 +1,19 @@
 let currentQuestion = 0;
+let correctAnswers = 0;
 
 showQuestion();
+
+//event
+document.querySelector('.scoreArea button').addEventListener('click', resetEvent);
 
 function showQuestion(){
 
     if(questions[currentQuestion]){
 
         let question = questions[currentQuestion];
-        console.log(question.question)
+
+        let porcentagem = (currentQuestion / questions.length) * 100;
+        document.querySelector('.progress--bar').style.width = `${porcentagem}%`
 
         document.querySelector('.scoreArea').style.display = 'none';
         document.querySelector('.questionArea').style.display = 'block';
@@ -24,9 +30,55 @@ function showQuestion(){
         document.querySelectorAll('.options .option').forEach(item => {
             item.addEventListener('click', optionClickEvent);
         })
+
+    }else{
+        finishQuiz();
     }
 }
 
 function optionClickEvent(e){
-    console.log("Clicou em ", e.target.getAttribute('data-op'))
+
+    let clickedOption = parseInt(e.target.getAttribute('data-op'));
+
+    if (questions[currentQuestion].answer === clickedOption) {
+
+        correctAnswers++;
+    }
+
+    currentQuestion++
+    showQuestion();
+
+}
+
+function finishQuiz(){
+
+    let points = (correctAnswers / questions.length) * 100;
+
+    document.querySelector('.scorePct').innerHTML = `Acertou ${points}%`;
+    document.querySelector('.scoreText2').innerHTML = `Você respondeu ${questions.length} questões e acertou ${correctAnswers} `
+
+    if(points < 30){
+        document.querySelector('.scoreText1').innerHTML = 'Tá ruim em?!';
+        document.querySelector('.scorePct').style.color = '#FF0000'
+    }
+    else if(points >= 30 && points < 70){
+        document.querySelector('.scoreText1').innerHTML = 'Muito bom!';
+        document.querySelector('.scorePct').style.color = '#FFFF00'  
+    }
+    else if(points >= 70){
+        document.querySelector('.scoreText1').innerHTML = 'Parabéns !!';
+        document.querySelector('.scorePct').style.color = '#0D630D'  
+    }
+
+    document.querySelector('.scoreArea').style.display = 'block';
+    document.querySelector('.questionArea').style.display = 'none';
+    document.querySelector('.progress--bar').style.width = '100%';
+
+
+}
+
+function resetEvent(){
+    correctAnswers = 0;
+    currentQuestion = 0;
+    showQuestion();
 }
